dd02137be8d99164b6fa542976c05a443ed2a6fd
Stephen Studer <stephenkstuder@gmail.com>
2022-05-02 10:14:50 -0700

    add about and photo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,11 @@
 
 <template>
   <main>
-    <h2>Homepage</h2>
+    <img src="../assets/stephen.jfif" alt="" />
+    <h2>I am a user interface engineer specialized in Javascript.</h2>
+    <p>
+      Experienced in intergations with api's and services, developing user
+      facing features, and implementing design systems.
+    </p>
   </main>
 </template>
