6a41d18c600c1eae62f871718879e92693509fb1
Stephen Studer <stephenkstuder@gmail.com>
2022-05-15 20:38:39 -0700

    add markdown support

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,7 +1,15 @@
-<script setup></script>
+<script>
+import FirstBlog from "../assets/blogs/example-blog.md";
+export default {
+  components: {
+    FirstBlog,
+  },
+};
+</script>
 
 <template>
   <main>
     <h2>Blog</h2>
+    <FirstBlog />
   </main>
 </template>
