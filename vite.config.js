a43290d841332d183d77a1271d3c9dc6b5036cfc
Stephen Studer <stephenkstuder@gmail.com>
2022-05-22 20:35:27 -0700

    pwa and alt

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,12 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Markdown from "vite-plugin-md";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    VitePWA({}),
     vue({
       include: [/\.vue$/, /\.md$/],
     }),
