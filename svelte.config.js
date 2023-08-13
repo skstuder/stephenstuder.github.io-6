cd8273c0e7041d2087b73d80ff8ada1f198c1d8c
Stephen Studer <stephenkstuder@gmail.com>
2023-08-13 15:10:29 -0700

    Fix dependency issues, remove eslint for now, remove old md preprocessor

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,8 @@
 import adapter from '@sveltejs/adapter-netlify';
-import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', '.svx', '.md'],
-	preprocess: mdsvex({ extensions: ['.svx', '.md'] }),
 	kit: {
 		adapter: adapter()
 	}
